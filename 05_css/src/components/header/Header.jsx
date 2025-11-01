865133588f844b54bad1a12b3d3fd2b911b0ae19
akash <akashsehgal2005@gmail.com>
2025-11-01 16:13:47 +0530

    intial commit

---
+++ 05_css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h1>hlo</h1>
+        <button className={styles.btn}>log in</button>
+    </div>
+  )
+}
+
+export default Header
