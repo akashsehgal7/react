3a67df071147b8f06b66b19c56b9d285aa2594b9
akash <akashsehgal2005@gmail.com>
2025-11-07 22:30:04 +0530

    initial commit

---
+++ 16_useeffect/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(10)
+  useEffect(function(){
+    console.log("use effect is running");
+    
+  },[num])
+  
+  return (
+    <div>
+      <h1>{num} </h1>
+      <h1>{num2} </h1>
+      <button onMouseEnter={()=> {
+        setNum(num + 1)
+      }} onMouseLeave={()=> {
+        setNum2(num2 + 10)
+      }} >Click</button>
+    </div>
+  )
+}
+
+export default App
