a89b2b907ed3dd324cf1f0f38803b2df132db720
akash <akashsehgal2005@gmail.com>
2025-11-11 16:17:20 +0530

    initial commit

---
+++ 18_react_router_dom/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nevbar = () => {
+  return (
+    <div className='nav'>
+        <h3>sheryians</h3>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/About'>About</Link>
+          <Link to='/Contact'>Contact</Link>
+          <Link to='/Product'>Product</Link>
+        </div>
+      
+      </div>
+  )
+}
+
+export default Nevbar
